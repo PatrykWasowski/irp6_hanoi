efc82ebd619dc2033bd7549a9546de6bff977e6f
PatrykWasowski <patryk.wasowski10@gmail.com>
2016-01-26 14:35:22 +0100

    Testy, stan na 26.01

--- src/irp6_hanoi.py
+++ src/irp6_hanoi.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import rospy
 from irpos import *
 from hanoi import *
 from std_msgs.msg import *
@@ -10,77 +11,110 @@ YELLOW_DISK = 0.072
 GREEN_DISK = 0.078
 BLUE_DISK = 0.082
 MAX_DISK_WIDTH = 0.082	#  maximal disk width in metres
-DISK_STOCK = 0.005	# zapas na szerokość chwytaka przy podejmowaniu krążka odpowiedniego koloru
-
+DISK_STOCK = 0.0	# zapas na szerokosc chwytaka przy podejmowaniu krazka odpowiedniego koloru
+is_above = 0
+move = [0, 0]
+starting_rod = -1000
+running = 0
 
 class Irp6Hanoi:
 	irpos = IRPOS("IRpOS", "Irp6p", 6, "irp6p_manager")
-	starting_rod = -1000
-	pub = rospy.Publisher('wanted_rod', String, queue_size = 5)  # pozniej uzywamy pub.publish("LEFT"/"RIGHT"/"CENTER")
-	sub_red = rospy.Subscriber('red_disk', Int16, callback_red)
-	sub_rod = rospy.Subscriber('rods', Int16MultiArray, callback_rod) 
-	rod_position[LEFT] = [0, 0, 0, 0, 0, 0]	
-	rod_position[CENTER] = [0, 0, 0, 0, 0, 0]	# position of IRp over left, center and right rod
-	rod_position[RIGHT] = [0, 0, 0, 0, 0, 0]
+		
+	rod_position = [[0 for x in range(5)] for x in range(5)] 
+	rod_position[0] = [0, 0, 0, 0, 0, 0]	
+	rod_position[1] = [0, 0, 0, 0, 0, 0]	# position of IRp over left, center and right rod
+	rod_position[2] = [0, 0, 0, 0, 0, 0]
 	current_rod = -1
-	move = [0, 0]
-	is_above = 0
-	step_list = []	
+	step_list = []		
+	pub = rospy.Publisher('wanted_rod', String, queue_size = 5)  # pozniej uzywamy pub.publish("LEFT"/"RIGHT"/"CENTER")
+	
+
+	def callback_red(data):
+		global starting_rod		
+		if(starting_rod == -1000):		
+			if(data.data > 200):
+				starting_rod = LEFT
+			elif(data.data < -300):
+				starting_rod = RIGHT
+			else:
+				starting_rod = CENTER
+
+			#sub_red.shutdown()		# there is no point in further subscribing this topic
+			print "Znaleziono czerwony krazek, jest on na pozycji " + str(starting_rod)
+
+	def callback_rod(data):
+		global is_above
+		global move
+		global running
+
+		if(abs(data.data[0]) < 0.002 and abs(data.data[1]) < 0.002 and running == 0):
+			is_above = 1
+			print "Jest nad"
+		else:
+			move = [data.data[0], data.data[1]]
+	
+
+	sub_red = rospy.Subscriber('red_disk', Int16, callback_red)
+	sub_rod = rospy.Subscriber('rods', Float32MultiArray, callback_rod) 
 
 	def set_step_list(self, steps):
 		step_list = steps
 
 	def move_to_starting_pose(self):
 		self.irpos.move_to_joint_position(STARTING_POSE, 15.0)
-		self.irpos.tfg_to_joint_position(MAX_DISK_WIDTH+DISK_STOCK, 3.0)
+		self.irpos.tfg_to_joint_position(RED_DISK+0.005, 3.0)
 		#self.irpos.tfg_to_joint_position(0.087, 5.0)
 		#self.irpos.move_rel_to_cartesian_pose_with_contact(20.0, Pose(Point(0.0, 0.0, 0.11), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(0.0,0.0,8.0),Vector3(0.0,0.0,0.0)))
 		#self.irpos.move_to_joint_position(STARTING_POSE, 15.0)
 				
-	def callback_red(data):
-		if(data.data > 200):
-			self.starting_rod = LEFT
-		elif(data.data < -300):
-			self.starting_rod = RIGHT
-		else:
-			self.starting_rod = CENTER
-
-		self.sub_red.shutdown()		# there is no point in further subscribing this topic
-
-	def callback_rod(data):
-		if(data.data[0] < 3 && data.data[1] < 3):
-			is_above = 1
-		else:
-			move = [data.data[0], data.data[1]]	
+	
 
 	def got_starting_rod(self):
+		global starting_rod		
 		if(starting_rod == -1000):
 			return 0
 		else:
-			if(self.starting_rod == LEFT):
+			if(starting_rod == LEFT):
 				self.irpos.move_rel_to_cartesian_pose(6.0, Pose(Point(0.0, -0.06, 0.0), Quaternion(0.0, 0.0, 0.0, 1.0)))
-			elif(self.starting_rod == RIGHT):
+			elif(starting_rod == RIGHT):
 				self.irpos.move_rel_to_cartesian_pose(6.0, Pose(Point(0.0, 0.06, 0.0), Quaternion(0.0, 0.0, 0.0, 1.0)))
-			self.current_rod = self.starting_rod 				
+			self.current_rod = starting_rod 				
 			
 			return 1
 	
-	def move_to_rod(self, end):
+	def move_to_rod(self, end):		
+		global is_above
+		global move	
+		global running	
+
+		print 'move_to_rod(' + str(end) + ")"
 		if(self.rod_position[end] != [0, 0, 0, 0, 0, 0]):
 			self.irpos.move_to_joint_position(rod_position[end])	# when position of rod is known already
-			self.current_rod = end
-			return 1
+			print "Pozycja juz jest znana, wiec sprobuje dawnego zapisu"
 		else:
-			if(self.is_above):
-				self.is_above = 0
-				self.rod_position[end] = self.irpos.get_joint_position()
-				self.move = [0, 0]
-				self.current_rod = end
-				return 1
-			elif(!self.is_above && move != [0, 0]):
-				self.irpos.move_rel_to_cartesian_pose(3.0, Pose(Point(0.0, self.move(0) / 43.0, 0.0), Quaternion(0.0, 0.0, 0.0, 1.0)))
-				self.move = [0, 0]
-				return 0
+			diff = (end - self.current_rod) * 0.074 
+			self.irpos.move_rel_to_cartesian_pose(7.0,  Pose(Point(0.0, diff, 0.0), Quaternion(0.0, 0.0, 0.0, 1.0)))
+			print "Jeszcze nie znam pozycji, probuje na oko przesunac " + str(diff) 
+					
+		
+		while (not is_above):
+			if((not is_above) and (move != [0, 0])):
+				running = 1				
+				self.irpos.move_rel_to_cartesian_pose(3.0, Pose(Point(-1*move[1], move[0], 0.0), Quaternion(0.0, 0.0, 0.0, 1.0)))
+				print "Przesuwam " + str(move[1]) + " i " + str(move[0])								
+				move = [0, 0]
+				running = 0
+				print "Czekam sekunde"
+				rospy.sleep(1.)
+			
+		
+		if(is_above):
+			is_above = 0
+			self.rod_position[end] = self.irpos.get_joint_position()
+			move = [0, 0]
+			self.current_rod = end
+			print "Dotarlem nad"		
+		
 
 	def get_disk_size(self, disk):
 		if(disk == 1):
@@ -92,33 +126,55 @@ class Irp6Hanoi:
 		elif(disk == 4):
 			return BLUE_DISK
 
+	def rod_name_from_nr(self, nr):
+		if(nr == 0):
+			return "LEFT"
+		elif(nr == 1):
+			return "CENTER"
+		elif(nr == 2):
+			return "RIGHT"
+
 	def grab_disk(self, disk, disks_below):
+		print "Pobieram dysk " + str(disk)
 		distance_to_disk = 12 - 1.4 - 1.0 * disks_below
-		self.irpos.move_rel_to_joint_position_with_contact(5.0, Pose(Point(0.0, 0.0, distance_to_disk), Quaternion(0.0, 0.0, 0.0, 1.0)))
-		self.irpos.tfg_to_joint_position(self.get_disk_size(disk), 3.0)
-		self.irpos.move_rel_to_joint_position(5.0, Pose(Point(0.0, 0.0, (-1.0)*distance_to_disk), Quaternion(0.0, 0.0, 0.0, 1.0)))
+		print "Musze zjechac na dol o " + str(distance_to_disk)
+		self.irpos.move_rel_to_cartesian_pose_with_contact(5.0, Pose(Point(0.0, 0.0, distance_to_disk), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(0.0,0.0,8.0),Vector3(0.0,0.0,0.0)))
+		#self.irpos.tfg_to_joint_position(self.get_disk_size(disk), 3.0)
+		print "Zlapalem dysk"
+		#self.irpos.move_rel_to_cartesian_pose(5.0, Pose(Point(0.0, 0.0, (-1.0)*distance_to_disk), Quaternion(0.0, 0.0, 0.0, 1.0)))
 					
 	def leave_disk(self):
-		self.irpos.move_rel_to_joint_position_with_contact(5.0, Pose(Point(0.0, 0.0, 2.0), Quaternion(0.0, 0.0, 0.0, 1.0)))
+		print "Zrzucam dysk"
+		self.irpos.move_rel_to_cartesian_pose_with_contact(5.0, Pose(Point(0.0, 0.0, 2.0), Quaternion(0.0, 0.0, 0.0, 1.0)))
 		self.irpos.tfg_to_joint_position(MAX_DISK_WIDTH+DISK_STOCK, 3.0)
-		self.irpos.move_rel_to_joint_position_with_contact(5.0, Pose(Point(0.0, 0.0, -2.0), Quaternion(0.0, 0.0, 0.0, 1.0)))
+		print "Upuscilem dysk"
+		self.irpos.move_rel_to_cartesian_pose_with_contact(5.0, Pose(Point(0.0, 0.0, -2.0), Quaternion(0.0, 0.0, 0.0, 1.0)))
 
 	def shift_disk(self, src, dst, disk, disks_below):
-		ready = 0		
-		while(!ready)
-			ready = self.move_to_rod(src) 
-
+		self.pub.publish(src)
+		rospy.sleep(1.)		# to be sure that new rod is found
+		
+		move_to_rod(src) 	
+		
+		self.pub.publish(dst)			# before robot moves to destination rod, the rod is already found 
 		self.grab_disk(disk, disks_below)
 		
-		ready = 0
-		while(!ready)
-			ready = self.move_to_rod(dst)
+		move_to_rod(dst)
 
 		self.leave_disk()	
 
 	def solve_hanoi(self):
-		for i in range(0, len(self.step_list)):
-			self.shift_disk(self.step_list[i][0], self.step_list[i][1], self.step_list[i][2], self.step_list[i][3])
+		global starting_rod
+		print "Zaczynam rozwiazywac Hanoi"
+		self.pub.publish(self.rod_name_from_nr(starting_rod))
+		self.move_to_rod(starting_rod)	
+		rospy.sleep(2.)	
+		self.grab_disk(1, 3)
+
+		#for i in range(0, len(self.step_list)):
+		#	self.shift_disk(self.step_list[i][0], self.step_list[i][1], self.step_list[i][2], self.step_list[i][3])
+		print "FINITO"		
+
 
 if __name__ == '__main__':
 #	irpos = IRPOS("IRpOS", "Irp6p", 6, "irp6p_manager")
@@ -128,10 +184,16 @@ if __name__ == '__main__':
 	obj.show_step_list()
 
 	irp = Irp6Hanoi()
+	print "Utworzono obiekt Irp6Hanoi"
 	irp.set_step_list(obj.get_step_list())
-	irp.move_to_starting_pose()
-	while(!got_starting_rod()):
-		print "Starting rod not detected\n"
+	print "Zdobylem liste krokow"
+	rospy.sleep(2.)
+	#irp.move_to_starting_pose()
+	#print "Przesunalem sie do pozycji poczatkowej"
+	if(not irp.got_starting_rod()):
+		print "Nie znalazlem czerwonego krazka"	
+	
+	irp.solve_hanoi()
 	
 	#ROZPOCZNIJ ROZWIAZYWANIE WIEZY HANOI
 		
